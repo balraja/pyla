a1a8bd8fd3c4ab527fce24d21bc255bf671c5ca8
Balraja Subbiah <balrajasubbiah@gmail.com>
2018-04-01 10:29:59 +0530

    Added support for projecting one vector over other

--- src/org/pyla/vectors/vector.py
+++ src/org/pyla/vectors/vector.py
@@ -17,7 +17,16 @@ class Vector(object):
         if (np_array.ndim != 1):
             raise "The underlying arrays of vectors should be a single dimensional"
         self.__array = np_array
+        self.__length = math.sqrt(np.sum(np.multiply(self.__array, self.__array)))
 
+    @property
+    def numpy_array(self):
+        """
+        :return: Returns underlying numpy rray
+        """
+        return self.__array
+
+    @property
     def length(self):
         """
         Returns length of a vector. The length of a vector is obtained as
@@ -25,14 +34,12 @@ class Vector(object):
 
         :return: mathematical length of a vector.
         """
-        return math.sqrt(np.sum(np.multiply(self.__array, self.__array)))
+        return self.__length
 
-    def dot_product(self, other_vector):
+    def __validate_other_operand(self, other_vector):
         """
-        Defines mathematical dot products between vectors
-
-        :param other_vector: The other vector that's participating in the dot product
-        :return: A scalar representing the dot product between vectors
+        A private method to check other vector that's a parameter
+        of binary operations on vectors
         """
         if not isinstance(other_vector, Vector):
             raise "The other_vector is not of the Vector type"
@@ -43,6 +50,70 @@ class Vector(object):
         if (self.__array.dtype != other_vector.__array.dtype):
             raise "The type of arrays in vectors don't match"
 
+
+    def add(self, other_vector):
+        """
+        Defines mathematical vector addition, wherein sum of a vector is
+        calculated as sum of its components.
+
+        :param other_vector: The vector to be summed up with this vector.
+        :return: A vector that's sum of the components of two vectors
+        """
+        self.__validate_other_operand(other_vector)
+        return Vector(np.add(self.__array, other_vector.__array))
+
+    def scalar_multiplication(self, scalar):
+        """
+        Defines scalar multiplication of a vector wherein a given scalar
+        is multiplied with the components of a vector.
+
+        :param scalar: The scalar with which the multiplication happens.
+        :return: A vector whose components are multiplied with scalar
+        """
+        return Vector(self.__array * scalar)
+
+    def unit_vector(self):
+        """
+        :return: Returns an unit length vector of itself, i.e. vector / length
+        """
+        return Vector(self.__array / self.__length)
+
+    def dot_product(self, other_vector):
+        """
+        Defines mathematical dot products between vectors
+
+        :param other_vector: The other vector that's participating in the dot product
+        :return: A scalar representing the dot product between vectors
+        """
+        self.__validate_other_operand(other_vector)
+
         return np.sum(np.multiply(self.__array, other_vector.__array))
 
+    def scalar_projection(self, projected_vector):
+        """
+        This method returns the scalar length of projection of provided vector
+        into this vector
 
+        :param projected_vector: The vector that's projected on to this vector
+        :return: the scalar length of projection
+        """
+
+        dot_product = self.dot_product(projected_vector)
+        return dot_product / self.__length
+
+    def vector_projection(self, projected_vector):
+        """
+        This method returns the projection of another vector onto this vector as a
+        vector in the same direction as this vector
+
+        :param projected_vector: The vector that's projected on to this vector
+        :return: The vector that defines the projection
+        """
+        scalar_projection = self.scalar_projection(projected_vector)
+        return self.unit_vector().scalar_multiplication(scalar_projection)
+
+    def __repr__(self):
+        return repr(self.__array)
+
+    def __str__(self):
+        return str(self.__array)
--- src/org/pyla/vectors/vector_test.py
+++ src/org/pyla/vectors/vector_test.py
@@ -15,7 +15,7 @@ class VectorTest(unittest.TestCase):
         Unit test for length function in vector class.
         """
         test_vector = vector.Vector(np.arange(5))
-        self.assertEqual(test_vector.length(), math.sqrt(30))
+        self.assertEqual(test_vector.length, math.sqrt(30))
 
     def test_dot_product(self):
         """
@@ -25,6 +25,17 @@ class VectorTest(unittest.TestCase):
         test_vector2 = vector.Vector(np.arange(5))
 
         self.assertEqual(test_vector1.dot_product(test_vector2), 30)
+
+    def test_projection(self):
+        """
+        Unit test that defines the projections
+        """
+        test_vector = vector.Vector(np.arange(5))
+        projected_vector = vector.Vector(np.ones(5, dtype=np.int64))
+        self.assertEqual(test_vector.scalar_projection(projected_vector), 10 / math.sqrt(30))
+
+        projection_as_vector = test_vector.vector_projection(projected_vector)
+        self.assertTrue(np.all(projection_as_vector.numpy_array == 10/30 * np.arange(5)))
 
 
 if __name__ == '__main__':
